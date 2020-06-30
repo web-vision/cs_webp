27fd521d952e7c328f218512f87173482aff1b2e
Anu <anu@web-vision.de>
2020-06-30 11:15:12 +0200

    [TASK] Initial commit Relates: CWDEV-234

---
+++ Classes/Service/OptimizeImageService.php
@@ -0,0 +1,46 @@
+<?php
+namespace Clickstorm\CsWebp\Service;
+
+use TYPO3\CMS\Core\Utility\CommandUtility;
+use TYPO3\CMS\Core\Utility\MathUtility;
+
+class OptimizeImageService {
+
+    /**
+     * Initialize
+     */
+    public function __construct() {
+        $this->configuration = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['cs_webp']);
+    }
+
+	/**
+	 * Perform image optimization
+	 *
+	 * @param string $file
+	 * @param string $extension
+	 */
+	public function process($file, $extension = NULL) {
+		if ($extension === NULL) {
+			$pathinfo = pathinfo($file);
+			if ($pathinfo['extension'] !== NULL) {
+				$extension = $pathinfo['extension'];
+			}
+		}
+		$extension = strtolower($extension);
+
+		if (($extension == 'jpg' || $extension == 'jpeg' || $extension == 'png') && strpos($file, 'fileadmin/_processed_') !== false) {
+            $webpfile = str_replace("." . $extension, ".webp", $file);
+            $quality = MathUtility::forceIntegerInRange($this->configuration['quality'],1,100);
+            $command = sprintf('convert %s -quality %s -define webp:lossless=true %s', $file, $quality, $webpfile);
+		}
+
+        if (isset($command)) {
+            $output = [];
+            $returnValue = 0;
+            CommandUtility::exec($command, $output, $returnValue);
+            if ((bool)$this->configuration['debug'] === TRUE && is_object($GLOBALS['BE_USER'])) {
+                $GLOBALS['BE_USER']->simplelog($command . ' exited with ' . $returnValue . '. Output was: ' . implode(' ', $output), 'cs_webp', 0);
+            }
+        }
+	}
+}
